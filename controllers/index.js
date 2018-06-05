17216b649a8a89a678523b9a5ff5a1a7f5e21677
KimSejun <cent@naver.com>
2018-06-05 15:49:46 +0900

    Completed the basic implementation.

--- controllers/index.js
+++ controllers/index.js
@@ -5,28 +5,32 @@ module.exports = function (app) {
 	var configureWeb3 = app.configureWeb3;
 	var validateCaptcha = app.validateCaptcha;
 
-	app.post('/', async function(request, response) {
+	app.post('/', async function (request, response) {
 		console.log("REQUEST:")
 		console.log(request.body)
 		var recaptureResponse = request.body["g-recaptcha-response"];
 		//console.log("recaptureResponse: ", recaptureResponse)
-		if (!recaptureResponse) return generateErrorResponse(response, {code: 500, title: "Error", message: "Invalid captcha"});
+		if (!recaptureResponse) return generateErrorResponse(response, {
+			code: 500,
+			title: "Error",
+			message: "Invalid captcha"
+		});
 
 		var receiver = request.body.receiver;
 		let out;
 		try {
 			out = await validateCaptcha(recaptureResponse);
-		} catch(e) {
+		} catch (e) {
 			return generateErrorResponse(response, e);
 		}
 		await validateCaptchaResponse(out, receiver, response);
 	});
 
-	app.get('/health', async function(request, response) {
+	app.get('/health', async function (request, response) {
 		let web3;
 		try {
 			web3 = await configureWeb3(config);
-		} catch(e) {
+		} catch (e) {
 			return generateErrorResponse(response, e);
 		}
 		let resp = {};
@@ -39,13 +43,21 @@ module.exports = function (app) {
 	});
 
 	async function validateCaptchaResponse(out, receiver, response) {
-		if (!out) return generateErrorResponse(response, {code: 500, title: "Error", message: "Invalid captcha"});
-		if (!out.success) return generateErrorResponse(response, {code: 500, title: "Error", message: "Invalid captcha"});
+		if (!out) return generateErrorResponse(response, {
+			code: 500,
+			title: "Error",
+			message: "Invalid captcha"
+		});
+		if (!out.success) return generateErrorResponse(response, {
+			code: 500,
+			title: "Error",
+			message: "Invalid captcha"
+		});
 
 		let web3;
 		try {
 			web3 = await configureWeb3(config);
-		} catch(e) {
+		} catch (e) {
 			return generateErrorResponse(response, e);
 		}
 		await sendPOAToRecipient(web3, receiver, response);
@@ -53,57 +65,74 @@ module.exports = function (app) {
 
 	async function sendPOAToRecipient(web3, receiver, response) {
 		let senderPrivateKey = config.Ethereum[config.environment].privateKey;
-		const privateKeyHex = Buffer.from(senderPrivateKey, 'hex')
-		if (!web3.utils.isAddress(receiver)) return generateErrorResponse(response, {code: 500, title: "Error", message: "invalid address"});
-		
-		const gasPrice = web3.utils.toWei('1', 'gwei')
-		let gasPriceHex = web3.utils.toHex(gasPrice);
+		const privateKeyHex = Buffer.from(senderPrivateKey, 'hex');
+		if (!web3.utils.isAddress(receiver)) return generateErrorResponse(response, {
+			code: 500,
+			title: "Error",
+			message: "invalid address"
+		});
+
+		const gasPrice = web3.utils.toWei('1', 'gwei');
+		//let gasPriceHex = web3.utils.toHex(gasPrice);
 		let gasLimitHex = web3.utils.toHex(config.Ethereum.gasLimit);
-		let nonce = await web3.eth.getTransactionCount(config.Ethereum[config.environment].account);
+		const account_address = config.Ethereum[config.environment].account;
+		//let account = web3.eth.accounts.privateKeyToAccount(senderPrivateKey);
+		let nonce = await web3.eth.getTransactionCount(account_address);
 		let nonceHex = web3.utils.toHex(nonce);
-		let BN = web3.utils.BN;
-		let ethToSend = web3.utils.toWei(new BN(config.Ethereum.milliEtherToTransfer), "milliether");
-		const rawTx = {
-		  nonce: nonceHex,
-		  gasPrice: gasPriceHex,
-		  gasLimit: gasLimitHex,
-		  to: receiver, 
-		  value: ethToSend,
-		  data: '0x00'
-		};
+		let ethToSend = web3.utils.toHex(web3.utils.toWei(config.Ethereum.EtherToTransfer, "ether"));
+		let chainId = web3.utils.toHex(131102);
 
-		let tx = new EthereumTx(rawTx);
-		tx.sign(privateKeyHex);
+		web3.eth.getGasPrice(function (e, r) {
+			let gasPriceHex = web3.utils.toHex(r);
+			const rawTx = {
+				nonce: nonceHex,
+				gasPrice: gasPriceHex,
+				gasLimit: gasLimitHex,
+				from: account_address,
+				to: receiver,
+				value: ethToSend,
+				data: '0x00',
+				chainId: chainId
+			};
+			//console.log(r);
+			let tx = new EthereumTx(rawTx);
+			tx.sign(privateKeyHex);
 
-		let serializedTx = tx.serialize();
+			let serializedTx = tx.serialize();
 
-		let txHash;
-		web3.eth.sendSignedTransaction("0x" + serializedTx.toString('hex'))
-		.on('transactionHash', (_txHash) => {
-			txHash = _txHash;
-		})
-		.on('receipt', (receipt) => {
-			console.log(receipt);
-			if (receipt.status == '0x1') {
-				return sendRawTransactionResponse(txHash, response);
-			} else {
-				let err = {code: 500, message: 'Transaction is mined, but status is false'};
-				return generateErrorResponse(response, err);
-			}
-		})
-		.on('error', (err) => {
-			return generateErrorResponse(response, err);
+			let txHash;
+			web3.eth.sendSignedTransaction("0x" + serializedTx.toString('hex'))
+				.on('transactionHash', (_txHash) => {
+					txHash = _txHash;
+				})
+				.on('receipt', (receipt) => {
+					console.log(receipt);
+					if (receipt.status == '0x1') {
+						return sendRawTransactionResponse(txHash, response);
+					} else {
+						let err = {
+							code: 500,
+							message: 'Transaction is mined, but status is false'
+						};
+						return generateErrorResponse(response, err);
+					}
+				})
+				.on('error', (err) => {
+					return generateErrorResponse(response, err);
+				});
 		});
 	}
 
 	function sendRawTransactionResponse(txHash, response) {
 		var successResponse = {
-			code: 200, 
-			title: "Success", 
+			code: 200,
+			title: "Success",
 			message: "Tx is mined",
 			txHash: txHash
 		};
-	  	
-	  	response.send({ success: successResponse });
+
+		response.send({
+			success: successResponse
+		});
 	}
 }
--- index.js
+++ index.js
@@ -31,12 +31,12 @@ require('./helpers/captchaHelper')(app);
 require('./controllers/index')(app);
 
 app.get('/', function(request, response) {
-  response.send('Sokol POA Network faucet');
+  response.send('EtherSocialNetwork faucet');
 });
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3001));
 
 app.listen(app.get('port'), function () {
-    console.log('Sokol testnet POA Network faucet is running on port', app.get('port'));
+    console.log('EtherSocialNetwork faucet is running on port', app.get('port'));
 });
 
